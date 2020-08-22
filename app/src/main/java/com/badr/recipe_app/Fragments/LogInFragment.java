17a372a5562c4a9b7960a1c00078c417d8760a94
Badr Eddine <bin.benh@gmail.com>
2020-08-23 00:53:14 +0100

    added some navigation flow and some ui tweaks

--- app/src/main/java/com/badr/recipe_app/Fragments/LogInFragment.java
+++ app/src/main/java/com/badr/recipe_app/Fragments/LogInFragment.java
@@ -3,10 +3,13 @@ package com.badr.recipe_app.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.badr.recipe_app.R;
 
@@ -24,7 +27,20 @@ public class LogInFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false);
+
+        View rootView =  inflater.inflate(R.layout.fragment_log_in, container, false);
+
+        EditText email, password;
+        Button goToRegisterButton, logInButton;
+        email = rootView.findViewById(R.id.log_in_email_input);
+        password = rootView.findViewById(R.id.log_in_password_input);
+        goToRegisterButton = rootView.findViewById(R.id.go_to_register_button);
+        logInButton = rootView.findViewById(R.id.log_in_button);
+
+        goToRegisterButton.setOnClickListener(V->{
+            NavHostFragment.findNavController(this).navigate(LogInFragmentDirections.actionLogInFragmentToRegisterFragment());
+        });
+
+        return rootView;
     }
 }
--- app/src/main/java/com/badr/recipe_app/Fragments/RegisterFragment.java
+++ app/src/main/java/com/badr/recipe_app/Fragments/RegisterFragment.java
@@ -3,16 +3,19 @@ package com.badr.recipe_app.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.badr.recipe_app.R;
 
 public class RegisterFragment extends Fragment {
-
-
+    private EditText name, email, password;
+    private Button registerButton, goTologinButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,23 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_register, container, false);
+
+        name = rootView.findViewById(R.id.register_name_input);
+        email = rootView.findViewById(R.id.register_email_input);
+        password = rootView.findViewById(R.id.register_password_input);
+
+        registerButton = rootView.findViewById(R.id.register_button);
+        goTologinButton = rootView.findViewById(R.id.go_to_log_in_button);
+
+        goTologinButton.setOnClickListener(v->{
+            NavHostFragment.findNavController(this).navigate(RegisterFragmentDirections.actionRegisterFragmentToLogInFragment());
+        });
+
+        registerButton.setOnClickListener(v -> {
+
+        });
+
+        return rootView;
     }
 }
